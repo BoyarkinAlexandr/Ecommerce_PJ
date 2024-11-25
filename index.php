2157f1fa74080867e5db1ab39912afa2a4d24eed
Alexandr Boyarkin <alexandrboyarkin@MacBook-Air-Alexandr.local>
2024-11-25 19:27:20 +0300

    Начало разработки

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav>
+        <label>АВТОСТРАХОВАНИЕ</label>
+
+        <ul>
+            <li>
+                <a href="#">Домой</a>
+            </li>
+
+            <li>
+                <a href="#">Продукты</a>
+            </li>
+
+            <li>
+                <a href="#">Контактые данные</a>
+            </li>
+
+            <li>
+                <a href="#">Регистрация</a>
+            </li>
+
+            <li>
+                <a href="#">Вход</a>
+            </li>
+
+
+
+        </ul>
+
+
+
+    </nav>
+
+    
+
+    
+    
+</body>
+</html>
